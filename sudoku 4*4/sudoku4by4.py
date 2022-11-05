c9a2c47ad9b252c0b9921313fa79be26c90455bd
Priyanshu Singh Panda <79511589+PriyanshusinghPanda@users.noreply.github.com>
2022-11-05 22:44:43 +0530

    Update sudoku4by4.py

--- sudoku 4*4/sudoku4by4.py
+++ sudoku 4*4/sudoku4by4.py
@@ -1,13 +1,11 @@
+import random
+
 body = (1,2,3,4)
-global a,b,c,d
-a = [1,4,0,0]
-b = [0,2,0,0]
-c = [4,1,3,2]
-d = [2,3,1,4]
 
 def pg(olis,tem,cha,bha):
     if olis==a:
         ap=[]
+        pc=[]
         for x in tem:
             temp=[]
             if b[x]!=0:
@@ -22,14 +20,14 @@ def pg(olis,tem,cha,bha):
             for z in bha:
                 if z not in temp:
                     temp.append(z)
-
             for y in cha:
                 if y not in temp:
-                    a[x]=y
+                    olis[x]=random.choice(cha)
                     ap.append(y)
                     pass
     if olis==b:
         ap=[]
+        pc=[]
         for x in tem:
             temp=[]
             if a[x]!=0:
@@ -44,14 +42,14 @@ def pg(olis,tem,cha,bha):
             for z in bha:
                 if z not in temp:
                     temp.append(z)
-            
             for y in cha:
                 if y not in temp:
-                    b[x]=y
+                    olis[x]=random.choice(cha)
                     ap.append(y)
                     pass           
     if olis==c:
         ap=[]
+        pc=[]
         for x in tem:
             temp=[]
             if a[x]!=0:
@@ -66,14 +64,14 @@ def pg(olis,tem,cha,bha):
             for z in bha:
                 if z not in temp:
                     temp.append(z)
-            
             for y in cha:
                 if y not in temp:
-                    b[x]=y
+                    olis[x]=random.choice(cha)
                     ap.append(y)
                     pass
     if olis==d:
         ap=[]
+        pc=[]
         for x in tem:
             temp=[]
             if a[x]!=0:
@@ -88,10 +86,9 @@ def pg(olis,tem,cha,bha):
             for z in bha:
                 if z not in temp:
                     temp.append(z)
-            
             for y in cha:
                 if y not in temp:
-                    d[x]=y
+                    olis[x]=random.choice(cha)
                     ap.append(y)
                     pass
     return olis
@@ -109,17 +106,39 @@ def new(olis):
     for x in body:
         if x not in olis:
             cha.append(x)
+
+    #print(olis,"place vacant=tem",tem,"jo  number line me  posssible ha=cha",cha,"jo number pahale se ha=bha",bha)
     return pg(olis,tem,cha,bha) 
             
 
-while 0 in a or b or c or d:
-    if 0 in a:
-        a=new(a)
-    if 0 in b:
-        b=new(b)
-    if 0 in c:
-        c=new(c)
-    if 0 in d:
-        d=new(d)
-    break
+while True:
+    #global a,b,c,d
+    a1 = [1,0,0,0]
+    b1 = [0,2,0,0]
+    c1 = [0,1,3,2]
+    d1 = [2,3,1,4]
+    a=a1
+    b=b1
+    c=c1
+    d=d1
+    bz=[]
+    if 0 in a or b or c or d:
+        for mno in range(4):
+            if 0 in a:
+                a=new(a)
+            if 0 in b:
+                b=new(b)
+            if 0 in c:
+                c=new(c)
+            if 0 in d:
+                d=new(d)
+            e=a+b+c+d
+            if 0 not in e:
+                break
+            else :
+                pass
+        pass
+    e=a+b+c+d
+    if 0 not in e:
+        break
 print(a,b,c,d,sep="\n")
